df12619c30fd73bb67bd34fee78db7fffaa79e34
Aki.SHIMO <fezrestia@gmail.com>
2021-04-07 19:55:55 +0900

    FIX: Update firebase import method

--- src/auth/javascript/auth.tsx
+++ src/auth/javascript/auth.tsx
@@ -1,7 +1,7 @@
 import "../css/auth.scss";
 import * as $ from "jquery";
 
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/functions";
 
